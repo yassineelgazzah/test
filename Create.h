793648530ffbcd9d9c97556afb3f434e1f3065cc
yassineelgazzah <133581085+yassineelgazzah@users.noreply.github.com>
2023-05-27 12:44:43 +0200

    Delete Create.h

--- Create.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_C
-#define H_C
-struct Game;
-struct Game **fill(struct Game **p, struct Game **pa);
-void createM(struct Game **p, struct Game **pa);
-struct Game **create(struct Game **p);
-void createJ(struct Game **p);
-#endif
